2c5b7bc317720c83f83d9c76bb04a01bdd81e042
mamed126 <mamed_126@mail.ru>
2021-09-20 14:44:40 +0400

    added product modle

---
+++ MikroDataTransferAPI/MikroDataTransferAPI/Model/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MikroDataTransferAPI.Model
+{
+    [Table("Products")]
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string  Name { get; set; }
+    }
+}
